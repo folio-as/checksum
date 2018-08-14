bf269971864f615ec6713e4260bdb666fef36332
Karl Anders Øygard <karloygard@gmail.com>
2018-08-14 12:28:23 +0200

    Updated tests.

--- checksum_test.go
+++ checksum_test.go
@@ -12,13 +12,13 @@ func TestChecksum(t *testing.T) {
 	if !Mod11("01010750160") {
 		t.Errorf("failed")
 	}
-	if !CheckNorwegianSSN("01010750241") {
+	if !ValidateNorwegianSSN("01010750241") {
 		t.Errorf("failed")
 	}
 	if !Mod11("919638095") {
 		t.Errorf("failed")
 	}
-	if CheckNorwegianSSN("01010750161") {
+	if ValidateNorwegianSSN("01010750161") {
 		t.Errorf("failed")
 	}
 	if !Luhn("79927398713") {
@@ -27,10 +27,10 @@ func TestChecksum(t *testing.T) {
 	if !Mod11("0095636818940001513") {
 		t.Errorf("failed")
 	}
-	if !CheckKID("79927398713") {
+	if !ValidateKID("79927398713") {
 		t.Errorf("failed")
 	}
-	if !CheckKID("0095636818940001513") {
+	if !ValidateKID("0095636818940001513") {
 		t.Errorf("failed")
 	}
 }
